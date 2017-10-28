199fb195db36d7a729439712a2e8c0f21c89277d
Kevin <klei0229@gmail.com>
2017-10-28 08:27:31 -0400

    <added index and sign up function to routes>

--- routes.py
+++ routes.py
@@ -1,1 +1,25 @@
+from flask import Flask,render_template
+from forms import SignupForm
 
+
+app = Flask(__name__)
+
+
+app.secret_key = 'development-key'
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+
+@app.route("/signup" , methods = ['GET','POST'])
+def signup():
+	form = SignupForm()
+	if request.method == "POST":
+		return "Success!"
+	elif request.method =="GET":
+
+		return render_template('signup.html',form = form)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
