5f0e6d4da3bcff00b3560cd34dfa8960da8433a1
Kevin <klei0229@gmail.com>
2017-11-05 10:42:10 -0500

    fixed template html

---
+++ Improv_app/__init__.py
@@ -0,0 +1,3 @@
+from flask import Flask
+app = Flask('Improv_app')
+from Improv_app import routes
---
+++ Improv_app/routes.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request
+from signup import SignupForm
+from . import app
+
+app.secret_key = 'development-key'
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+
+@app.route("/signup" , methods = ['GET','POST'])
+def signup():
+	form = SignupForm()
+	if request.method == "POST":
+		return "Success!"
+	elif request.method =="GET":
+
+		return render_template('signup.html',form = form)
