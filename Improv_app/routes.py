2ecba6840a384c48fca13bc8266d31d5a496a8b6
Kevin <klei0229@gmail.com>
2017-11-07 08:23:04 -0500

    connected working postgres to add users at signup

--- Improv_app/routes.py
+++ Improv_app/routes.py
@@ -1,9 +1,17 @@
 from flask import Flask,render_template,request
 from .forms import SignupForm
+from .models import db, User
+
+
 #from . import signup 
+
 #from signup import SignupForm
 from . import app
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:xyz123890xyz@localhost:5432/learningflask'
+
+
+db.init_app(app)
 
 app.secret_key = 'development-key'
 @app.route("/")
@@ -17,7 +25,11 @@ def signup():
 	if request.method == "POST":
 		if form.validate() == False:
 			return render_template('signup.html',form = form)
+		else:
+			newuser = User(form.first_name.data, form.last_name.data , form.email.data, form.password.data)
 			
+			db.session.add(newuser)
+			db.session.commit()
 		return "Success!"
 	elif request.method =="GET":
 
