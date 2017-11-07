ca72cd04aeb55fde3e35479a927ab08fac9f78c5
Kevin <klei0229@gmail.com>
2017-11-07 09:21:12 -0500

    reverted last commit

--- signup.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField,PasswordField, SubmitField
-
-class SignupForm(Form):
-	first_name = StringField('First Name')
-	last_name = StringField('Last Name')
-	email = StringField('Email')
-	password = PasswordField('Password')
-	submit = SubmitField('Sign up')
