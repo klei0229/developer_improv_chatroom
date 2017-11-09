7237b3783f29fcc8f81aeaa5a40940fb4fc419ff
Kevin <klei0229@gmail.com>
2017-11-09 16:13:30 -0500

    week3

--- application.py
+++
@@ -1,8 +0,0 @@
-from improv_app import app as application
-<<<<<<< HEAD
-#from form import SignupForm
-=======
->>>>>>> 7db41aee23bbcc286bfe8e0f89141210a3b72bd8
-if __name__ == "__main__":
-    application.debug = True
-    application.run()
