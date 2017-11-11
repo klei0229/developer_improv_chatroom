8e4529b9f8a09da3473728da9c43492bf8756022
Kevin <klei0229@gmail.com>
2017-11-11 14:28:40 -0500

    removed merge errors

--- application.py
+++ application.py
@@ -1,9 +1,5 @@
 from improv_app import app as application
-<<<<<<< HEAD
-=======
-#from form import SignupForm
 
->>>>>>> 639aaef86a9b879b60af04fa29f568e6a4f48dca
 if __name__ == "__main__":
     application.debug = True
     application.run()
