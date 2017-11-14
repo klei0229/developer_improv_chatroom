1a4fbaac717e7b99d4be2b03c84116b6fb65be53
YuHaoLiu <liuyuhao717@gmail.com>
2017-11-14 15:46:06 -0500

    increase the pylint score from -4 to -3

--- improv_app/routes.py
+++ improv_app/routes.py
@@ -4,19 +4,20 @@ from .models import db, User
 from . import app
 
 #postgres sq
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:xyz123890xyz@localhost:5432/learningflask'
+postgresql_line = 'postgresql://postgres:xyz123890xyz@localhost:5432/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = postgresql_line
 db.init_app(app)
 #secretkey for login
 app.secret_key = 'development-key'
 @app.route("/")
 def index():
 	return render_template("index.html")
-@app.route("/signup" , methods = ['GET','POST'])
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
 	form = SignupForm()
 	if request.method == "POST":
 		if form.validate() == False:
-			return render_template('signup.html',form = form)
+			return render_template('signup.html', form = form)
 		else:
 			newuser = User(form.first_name.data, form.last_name.data , form.email.data, form.password.data)
 			db.session.add(newuser)
@@ -24,7 +25,7 @@ def signup():
 			#session['email'] = newuser.email
 			return redirect(url_for('index'))
 	elif request.method =="GET":
-		return render_template('signup.html',form = form)
+		return render_template('signup.html', form = form)
 
 #create a room decorator by jack
 @app.route("/create")
