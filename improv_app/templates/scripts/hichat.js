ef7d8b9f33dbafcc943abc56c4190d3d3e94ff5b
yejack <yeshuchuan000@gmail.com>
2017-11-26 14:13:04 -0500

    connect socket

--- improv_app/templates/scripts/hichat.js
+++ improv_app/templates/scripts/hichat.js
@@ -2,3 +2,10 @@ window.onload = function() {
     var hichat = new HiChat();
     hichat.init();
 };
+var HiChat = function() {
+    this.socket = null;
+};
+HiChat.prototype = {
+    init: function() {
+        var that = this;
+	 this.socket = io.connect();
