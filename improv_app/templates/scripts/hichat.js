b994dc2166f1301120de2dead26ceb9a0922ed2a
yejack <yeshuchuan000@gmail.com>
2017-11-26 14:23:12 -0500

    chatroom js

--- improv_app/templates/scripts/hichat.js
+++ improv_app/templates/scripts/hichat.js
@@ -99,3 +99,71 @@ HiChat.prototype = {
                 reader.readAsDataURL(file);
             };
         }, false);
+	this._initialEmoji();
+        document.getElementById('emoji').addEventListener('click', function(e) {
+            var emojiwrapper = document.getElementById('emojiWrapper');
+            emojiwrapper.style.display = 'block';
+            e.stopPropagation();
+        }, false);
+        document.body.addEventListener('click', function(e) {
+            var emojiwrapper = document.getElementById('emojiWrapper');
+            if (e.target != emojiwrapper) {
+                emojiwrapper.style.display = 'none';
+            };
+        });
+        document.getElementById('emojiWrapper').addEventListener('click', function(e) {
+            var target = e.target;
+            if (target.nodeName.toLowerCase() == 'img') {
+                var messageInput = document.getElementById('messageInput');
+                messageInput.focus();
+                messageInput.value = messageInput.value + '[emoji:' + target.title + ']';
+            };
+        }, false);
+    },
+    _initialEmoji: function() {
+        var emojiContainer = document.getElementById('emojiWrapper'),
+            docFragment = document.createDocumentFragment();
+        for (var i = 69; i > 0; i--) {
+            var emojiItem = document.createElement('img');
+            emojiItem.src = '../content/emoji/' + i + '.gif';
+            emojiItem.title = i;
+            docFragment.appendChild(emojiItem);
+        };
+        emojiContainer.appendChild(docFragment);
+    },
+    _displayNewMsg: function(user, msg, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8),
+            //determine whether the msg contains emoji
+            msg = this._showEmoji(msg);
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span>' + msg;
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    },
+    _displayImage: function(user, imgData, color) {
+        var container = document.getElementById('historyMsg'),
+            msgToDisplay = document.createElement('p'),
+            date = new Date().toTimeString().substr(0, 8);
+        msgToDisplay.style.color = color || '#000';
+        msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span> <br/>' + '<a href="' + imgData + '" target="_blank"><img src="' + imgData + '"/></a>';
+        container.appendChild(msgToDisplay);
+        container.scrollTop = container.scrollHeight;
+    },
+    _showEmoji: function(msg) {
+        var match, result = msg,
+            reg = /\[emoji:\d+\]/g,
+            emojiIndex,
+            totalEmojiNum = document.getElementById('emojiWrapper').children.length;
+        while (match = reg.exec(msg)) {
+            emojiIndex = match[0].slice(7, -1);
+            if (emojiIndex > totalEmojiNum) {
+                result = result.replace(match[0], '[X]');
+            } else {
+                result = result.replace(match[0], '<img class="emoji" src="../content/emoji/' + emojiIndex + '.gif" />');//todo:fix this in chrome it will cause a new request for the image
+            };
+        };
+        return result;
+    }
+};
